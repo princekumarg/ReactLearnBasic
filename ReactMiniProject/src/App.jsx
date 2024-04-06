ee1b7ce2150fecca1bccace8b26c68272109c03c
prince kumar <agarwal8789@gmail.com>
2024-04-06 15:10:45 +0530

    commit made

--- ReactMiniProject/src/App.jsx
+++ ReactMiniProject/src/App.jsx
@@ -1,10 +1,12 @@
 import BrithDayList from './BrithDayList/BrithDayList'
+import TourApp from './TourApp/TourApp'
 
 function App() {
 
   return (
     <div id="app">
       <BrithDayList/>
+      <TourApp/>
     </div>
   )
 }
