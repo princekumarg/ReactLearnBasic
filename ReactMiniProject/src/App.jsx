436e1c7fb3eefc0f38d70752052fc3a94c3e0217
prince kumar <agarwal8789@gmail.com>
2024-04-10 15:05:41 +0530

    commit made

--- ReactMiniProject/src/App.jsx
+++ ReactMiniProject/src/App.jsx
@@ -1,6 +1,7 @@
 import AccordianApp from './Accordian/AccordianApp'
 import BrithDayList from './BrithDayList/BrithDayList'
 import DinnerMenuApp from './DinnerMenu/DinnerMenuApp'
+import LoremIpsum from './LoremIpsum/LoremIpsum'
 import Review from './Review/Review'
 import TabsAPP from './TabsAPP/TabsAPP'
 import TourApp from './TourApp/TourApp'
@@ -15,6 +16,7 @@ function App() {
       <AccordianApp/>
       <DinnerMenuApp/>
       <TabsAPP/>
+      <LoremIpsum/>
     </div>
   )
 }
