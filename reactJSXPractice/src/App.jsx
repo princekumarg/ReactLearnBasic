d008ace0e2f4f56872421df2f313f5155891e107
prince kumar <agarwal8789@gmail.com>
2024-04-19 14:24:14 +0530

    commit made React all Pratice'

---
+++ reactJSXPractice/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Learnjsx } from "./assets/Component/learnjsx"
+import { Learncomp } from "./assets/Component/Learncomp"
+import { Learnprops } from "./assets/Component/Learnprops";
+import LearnLiftingSetup from "./assets/Component/LearnLiftingSetup";
+import LearnState from "./assets/Component/LearnState";
+import CounterApp from "./assets/Component/CounterApp";
+import LearnuseState from "./assets/Component/LearnuseState";
+function App() {
+  let price=10;
+  const getData=(data)=>{
+    console.log(data);
+  }
+  return (
+    <>
+      <h1>Hello Prince</h1>
+      <Learnjsx/>{/*This is a component*/}
+      <Learncomp/>{/*This is a component with variable*/}
+      <Learnprops name="Prince" price={price}/>
+      <LearnLiftingSetup myClick={getData}/>
+      <LearnState/>
+      <CounterApp/>
+      <LearnuseState/>
+    </>
+  )
+}
+
+export default App
